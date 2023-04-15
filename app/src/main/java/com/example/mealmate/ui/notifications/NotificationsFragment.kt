a4a129853b01f0a0f25d00e610f7fa3fc232e793
amoghkapalli <amoghkapalli12@gmail.com>
2023-04-14 22:45:35 -0700

    NotificationsFragment is updated along with search xml.
    Need to work on intent and api call.

--- app/src/main/java/com/example/mealmate/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/mealmate/ui/notifications/NotificationsFragment.kt
@@ -1,9 +1,12 @@
 package com.example.mealmate.ui.notifications
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -12,6 +15,8 @@ import com.example.mealmate.databinding.FragmentNotificationsBinding
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
+    private lateinit var searchButton: Button
+    private lateinit var searchEditText: EditText
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,16 +27,19 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
-
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        searchButton = binding.searchButton
+        searchEditText = binding.editText
+
+        searchButton.setOnClickListener {
+            val searchText = searchEditText.text.toString()
+            val intent = Intent(context, SearchResultActivity::class.java)
+            intent.putExtra("searchText", searchText)
+            startActivity(intent)
         }
+
         return root
     }
 
