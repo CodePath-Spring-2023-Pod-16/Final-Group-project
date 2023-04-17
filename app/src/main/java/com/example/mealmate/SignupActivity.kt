49dd5a4ee846a0022cc39531ed950b886b9446b8
Andrea Gonzalez <andreag4@usc.edu>
2023-04-17 01:17:50 -0700

    login and sign up design, firebase auth

---
+++ app/src/main/java/com/example/mealmate/SignupActivity.kt
@@ -0,0 +1,63 @@
+package com.example.mealmate
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class SignupActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().reference
+
+        findViewById<Button>(R.id.btn_signup).setOnClickListener {
+            val name = findViewById<EditText>(R.id.et_name).text.toString()
+            val email = findViewById<EditText>(R.id.et_email).text.toString()
+            val password = findViewById<EditText>(R.id.et_password).text.toString()
+
+            if (name.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            val userId = auth.currentUser!!.uid
+                            val user = User(name, email)
+
+                            database.child("users").child(userId).setValue(user)
+                                .addOnCompleteListener { task ->
+                                    if (task.isSuccessful) {
+                                        Toast.makeText(this, "Registration successful.", Toast.LENGTH_SHORT).show()
+                                        val intent = Intent(this, MainActivity::class.java)
+                                        startActivity(intent)
+                                        finish()
+                                    } else {
+                                        Toast.makeText(this, "Registration failed.", Toast.LENGTH_SHORT).show()
+                                    }
+                                }
+                        } else {
+                            Toast.makeText(this, "Registration failed.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this, "Please enter name, email, and password.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        findViewById<TextView>(R.id.tv_login).setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mealmate/User.kt
@@ -0,0 +1,4 @@
+package com.example.mealmate
+
+data class User(val name: String, val email: String)
+
