dc65bdd5a20293fa45de336a6eef0c339827bb6e
Andrea Gonzalez <andreag4@usc.edu>
2023-04-24 21:50:28 -0700

    favorites

--- app/src/main/java/com/example/mealmate/FavoriteActivity.kt
+++ app/src/main/java/com/example/mealmate/FavoriteActivity.kt
@@ -1,9 +1,16 @@
 package com.example.mealmate
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.RequestParams
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+import org.json.JSONException
+private const val TAG = "FavoriteActivity/"
 
 class FavoriteActivity : AppCompatActivity() {
     private lateinit var favoriteRecipeAdapter: FavoriteRecipeAdapter
@@ -27,7 +34,40 @@ class FavoriteActivity : AppCompatActivity() {
 
     // TODO: Implement the loadFavoriteRecipes function to load the favorite recipes with the recipeIds list
     private fun loadFavoriteRecipes(recipeIds: List<String>): List<FavoriteViewModel> {
-        // TODO: Implement this function to load the favorite recipes
-        return listOf()
+        val favoriteRecipes = mutableListOf<FavoriteViewModel>()
+
+        // For each recipeId in the list, fetch the recipe information using the Spoonacular API
+        recipeIds.forEach { recipeId ->
+            val client = AsyncHttpClient()
+            val RECIPE_URL = "https://api.spoonacular.com/recipes/$recipeId/information?apiKey=${API_KEY}"
+            val requestParams = RequestParams()
+            requestParams.put("apiKey", API_KEY)
+
+            client.get(RECIPE_URL, requestParams, object : JsonHttpResponseHandler() {
+                override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+                    try {
+                        // Parse the recipe information JSON and add the recipe to the list of favorite recipes
+                        val recipe = createJson().decodeFromString(FavoriteViewModel.serializer(), json.jsonObject.toString())
+                        favoriteRecipes.add(recipe)
+
+                        // Notify the adapter that the data has changed
+                        favoriteRecipeAdapter.notifyDataSetChanged()
+                    } catch (e: JSONException) {
+                        Log.e(TAG, "Exception: $e")
+                    }
+                }
+
+                override fun onFailure(
+                    statusCode: Int,
+                    headers: Headers?,
+                    response: String?,
+                    throwable: Throwable?
+                ) {
+                    Log.e(TAG, "Failed to fetch recipe: $response")
+                }
+            })
+        }
+
+        return favoriteRecipes
     }
 }
